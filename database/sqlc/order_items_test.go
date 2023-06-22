4b4925de53be1258598d02fd111fca2823dba433
Paul Odhiambo <paulodhiambo962@gmail.com>
2023-06-22 15:59:07 +0300

    orderitems model testcases

--- database/sqlc/order_items_test.go
+++ database/sqlc/order_items_test.go
@@ -1,1 +1,76 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"ecom/database/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestCreateOrderItem(t *testing.T) {
+	createRandomOrderItem(t)
+}
+
+func TestGetOrderItem(t *testing.T) {
+	orderItem1 := createRandomOrderItem(t)
+	orderItem2, err := testQueries.GetOrderItem(context.Background(), orderItem1.OrderID)
+	require.NoError(t, err)
+	require.Equal(t, orderItem1.OrderID, orderItem2.OrderID)
+	require.Equal(t, orderItem1.ProductID, orderItem2.ProductID)
+	require.Equal(t, orderItem1.Quantity, orderItem2.Quantity)
+}
+
+func TestDeleteOrderItem(t *testing.T) {
+	order := createRandomOrderItem(t)
+	err := testQueries.DeleteOrderItem(context.Background(), order.OrderID)
+	require.NoError(t, err)
+	category2, err := testQueries.GetOrderItem(context.Background(), order.OrderID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, category2)
+}
+
+func TestGetOrdersItem(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomOrderItem(t)
+	}
+	arg := ListOrderItemsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	ordersItems, err := testQueries.ListOrderItems(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, ordersItems, 5)
+
+	for _, orderItem := range ordersItems {
+		require.NotEmpty(t, orderItem)
+	}
+}
+
+func TestUpdateOrderItem(t *testing.T) {
+	order := createRandomOrderItem(t)
+
+	arg := UpdateOrderItemParams{
+		OrderID:  order.OrderID,
+		Quantity: int32(util.RandomInt(19, 99999)),
+	}
+	err := testQueries.UpdateOrderItem(context.Background(), arg)
+	require.NoError(t, err)
+}
+
+func createRandomOrderItem(t *testing.T) OrderItem {
+	order := createRandomOrder(t)
+	product := createRandomProduct(t)
+	arg := CreateOrderItemParams{
+		OrderID:   order.ID,
+		ProductID: product.ID,
+		Quantity:  int32(util.RandomInt(0, 999)),
+	}
+	orderItem, err := testQueries.CreateOrderItem(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, arg.OrderID, orderItem.OrderID)
+	require.Equal(t, arg.Quantity, orderItem.Quantity)
+	require.Equal(t, arg.ProductID, orderItem.ProductID)
+	return orderItem
+}
--- database/sqlc/orders_test.go
+++ database/sqlc/orders_test.go
@@ -1,1 +1,78 @@
 package database
+
+import (
+	"context"
+	"database/sql"
+	"ecom/database/util"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestCreateOrder(t *testing.T) {
+	createRandomOrder(t)
+}
+
+func TestGetOrder(t *testing.T) {
+	order1 := createRandomOrder(t)
+	order2, err := testQueries.GetOrder(context.Background(), order1.ID)
+	require.NoError(t, err)
+	require.Equal(t, order1.Status, order2.Status)
+	require.Equal(t, order1.ID, order2.ID)
+	require.Equal(t, order1.UserID, order2.UserID)
+}
+
+func TestDeleteOrder(t *testing.T) {
+	order := createRandomOrder(t)
+	err := testQueries.DeleteOrder(context.Background(), order.ID)
+	require.NoError(t, err)
+	category2, err := testQueries.GetOrder(context.Background(), order.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, category2)
+}
+
+func TestGetOrders(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomOrder(t)
+	}
+	arg := ListOrdersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	orders, err := testQueries.ListOrders(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, orders, 5)
+
+	for _, order := range orders {
+		require.NotEmpty(t, order)
+	}
+}
+
+func TestUpdateOrder(t *testing.T) {
+	order := createRandomOrder(t)
+
+	arg := UpdateOrderParams{
+		ID:     order.ID,
+		Status: util.RandomFullName(),
+	}
+	err := testQueries.UpdateOrder(context.Background(), arg)
+	require.NoError(t, err)
+}
+
+func createRandomOrder(t *testing.T) Order {
+	user := createRandomUser(t)
+	arg := CreateOrderParams{
+		ID:        util.RandomInt(1, 99999999),
+		UserID:    int32(user.ID),
+		Status:    util.RandomString(4),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	order, err := testQueries.CreateOrder(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, arg.Status, order.Status)
+	require.Equal(t, arg.ID, order.ID)
+	require.Equal(t, arg.UserID, order.UserID)
+	return order
+}
